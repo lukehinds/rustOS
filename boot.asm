01f61076218b2cca6102cfd912b72adbed4788a3
lukehinds <lukehinds@gmail.com>
2016-08-03 16:45:47 +0100

    Complete boot.asm

--- boot.asm
+++ boot.asm
@@ -46,6 +46,9 @@ start:
     or eax, 1 << 16
     mov cr0, eax
 
+    ; Load the GDT
+    lgdt [gdt64.pointer]
+
     ; print hello world
     mov word [0xb8000], 0x0248 ; L
     mov word [0xb8002], 0x0265 ; u
@@ -73,3 +76,25 @@ p3_table:
     resb 4096
 p2_table:
     resb 4096
+
+; GDT 
+section .rodata
+gdt64:
+    dq 0
+.code: equ $ - gdt64
+    dq (1<<44) | (1<<47) | (1<<41) | (1<<43) | (1<<53)
+.data: equ $ - gdt64
+    dq (1<<44) | (1<<47) | (1<<41)
+.pointer:
+    dw .pointer - gdt64 - 1
+    dq gdt64
+
+
+section .text
+bits 64
+long_mode_start:
+
+    mov rax, 0x2f592f412f4b2f4f
+    mov qword [0xb8000], rax
+
+    hlt
